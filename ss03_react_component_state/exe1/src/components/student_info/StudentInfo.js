249201a20ee30cd33338014666d45e20e3b60ee1
bondatfpt <bondatfpt@gmail.com>
2023-10-20 11:49:51 +0700

    ok

--- ss03_react_component_state/exe1/src/components/student_info/StudentInfo.js
+++ ss03_react_component_state/exe1/src/components/student_info/StudentInfo.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react'
  
 
 export default class StudentInfo extends Component {
-    constructor(props){
-        super(props);
-    }
   render() {
     return (
       <div>
