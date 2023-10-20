2b0ce60210953531b2da3c571f8f58f42239c39a
bondatfpt <bondatfpt@gmail.com>
2023-10-20 11:51:57 +0700

    ok

--- ss03_react_component_state/exe1/src/components/student_info/StudentInfo.js
+++ ss03_react_component_state/exe1/src/components/student_info/StudentInfo.js
@@ -1,36 +1,33 @@
-import React, { Component } from 'react'
- 
+import React, { Component } from "react";
 
 export default class StudentInfo extends Component {
-    constructor(props){
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
       <div>
-        <table className='table'>
-            <thead>
-                <th>Id</th>
-                <th>Name</th>
-                <th>Age</th>
-                <th>Address</th>
-            </thead>
-            <tbody>
-                {
-                    this.props.list.map(item => {
-                        return (
-                            <tr key={item.id}>
-                                <td>{item.id}</td>
-                                <td>{item.name}</td>
-                                <td>{item.age}</td>
-                                <td>{item.address}</td>
-                            </tr>
-                        )
-                    })
-                }
-            </tbody>
+        <table className="table">
+          <thead>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Address</th>
+          </thead>
+          <tbody>
+            {this.props.list.map((item) => {
+              return (
+                <tr key={item.id}>
+                  <td>{item.id}</td>
+                  <td>{item.name}</td>
+                  <td>{item.age}</td>
+                  <td>{item.address}</td>
+                </tr>
+              );
+            })}
+          </tbody>
         </table>
       </div>
-    )
+    );
   }
 }
